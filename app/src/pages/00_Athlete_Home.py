8be3a683cc21960793d5d2db7c894a5223bbeb64
jasendo <jack.senders@gmail.com>
2026-08-12 11:14:56 -0400

    init frontend work

---
+++ app/src/pages/00_Athlete_Home.py
@@ -0,0 +1,28 @@
+import logging
+logger = logging.getLogger(__name__)
+
+import streamlit as st
+from modules.nav import SideBarLinks
+
+st.set_page_config(layout='wide')
+
+# Show appropriate sidebar links for the role of the currently logged in user
+SideBarLinks()
+
+st.title(f"Welcome Elite Athlete, {st.session_state['first_name']}.")
+st.write('### What would you like to do today superstar?')
+
+if st.button('View Profile',
+             type='primary',
+             use_container_width=True):
+    st.switch_page('pages/01_Athlete_Profile.py')
+
+if st.button('View Rosters',
+             type='primary',
+             use_container_width=True):
+    st.switch_page('pages/02_View_Rosters.py')
+
+if st.button('Upload New Clips',
+             type='primary',
+             use_container_width=True):
+    st.switch_page('pages/03_Upload_Clips.py')
